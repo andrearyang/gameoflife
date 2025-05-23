3a263d17f45b040aabce6febe97b177c4d7b02bc
andrearyang <andreayang64@gmail.com>
2025-05-22 17:59:56 -0700

    pulsar

--- test.py
+++ test.py
@@ -11,15 +11,15 @@ def show_board(board, pause_time=0.5):
     plt.imshow(board, cmap='Greys', interpolation='nearest')
     plt.xticks([])
     plt.yticks([])
-    plt.grid(False)
+    plt.grid()
     plt.pause(pause_time)
     plt.clf() 
 
-board = load_board_state('/patterns/endless_life.txt')
+board = load_board_state('patterns/pulsar.txt')
 plt.figure()
 
 while True:
     os.system('cls' if os.name == 'nt' else 'clear')
     show_board(board)
     board = next_board_state(board)
-    time.sleep(0.2)
+    time.sleep(0.025)
